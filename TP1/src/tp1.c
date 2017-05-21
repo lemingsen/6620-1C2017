d0469a26fa055c1a4dcc603cc3237024a453a7af
Belu Beltran <belubeltran@gmail.com>
2017-05-20 21:55:56 -0300

    Decode (falta ver el padding)

--- TP1/src/tp1.c
+++ TP1/src/tp1.c
@@ -9,23 +9,41 @@ int main(int argc,  char* const* argv)
 	FILE *fpOut;
 	int fdIn, fdOut;
 
-	fpIn = fopen("input.txt", "r");
+	// fpIn = fopen("input_encode.txt", "r");
+	// if (fpIn != NULL)
+	// 	fdIn = fileno(fpIn);
+	// else {
+	// 	fprintf(stderr, "Error: input_encode.txt.\n");
+	// 	exit(1);
+	// }
+
+	// fpOut = fopen("output_encode.txt", "w");
+	// if (fpOut != NULL)
+	// 	fdOut = fileno(fpOut);
+	// else {
+	// 	fprintf(stderr, "Error: output_encode.txt.\n");
+	// 	exit(1);
+	// }
+
+	// base64_encode(fdIn, fdOut);
+
+	fpIn = fopen("input_decode.txt", "r");
 	if (fpIn != NULL)
 		fdIn = fileno(fpIn);
 	else {
-		fprintf(stderr, "Error: input.txt.\n");
+		fprintf(stderr, "Error: input_decode.txt.\n");
 		exit(1);
 	}
 
-	fpOut = fopen("output.txt", "w");
+	fpOut = fopen("output_decode.txt", "w");
 	if (fpOut != NULL)
 		fdOut = fileno(fpOut);
 	else {
-		fprintf(stderr, "Error: output.txt.\n");
+		fprintf(stderr, "Error: output_decode.txt.\n");
 		exit(1);
 	}
 
-	base64_encode(fdIn, fdOut);
+	base64_decode(fdIn, fdOut);
 
 	return 0;
 }
