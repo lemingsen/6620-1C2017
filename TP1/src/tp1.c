431a60469e6f0502f06d9bfe35bd455a468a9d07
Belu Beltran <belubeltran@gmail.com>
2017-05-22 23:21:53 -0300

    Cambios en lectura1

--- TP1/src/tp1.c
+++ TP1/src/tp1.c
@@ -9,41 +9,47 @@ int main(int argc,  char* const* argv)
 	FILE *fpOut;
 	int fdIn, fdOut;
 
-	// fpIn = fopen("input_encode.txt", "r");
-	// if (fpIn != NULL)
-	// 	fdIn = fileno(fpIn);
-	// else {
-	// 	fprintf(stderr, "Error: input_encode.txt.\n");
-	// 	exit(1);
-	// }
-
-	// fpOut = fopen("output_encode.txt", "w");
-	// if (fpOut != NULL)
-	// 	fdOut = fileno(fpOut);
-	// else {
-	// 	fprintf(stderr, "Error: output_encode.txt.\n");
-	// 	exit(1);
-	// }
-
-	// base64_encode(fdIn, fdOut);
-
-	fpIn = fopen("input_decode.txt", "r");
+	fpIn = fopen("input_encode.txt", "r");
 	if (fpIn != NULL)
 		fdIn = fileno(fpIn);
 	else {
-		fprintf(stderr, errmsg[0]);
+		fprintf(stderr, "Error: input_encode.txt.\n");
 		exit(1);
 	}
 
-	fpOut = fopen("output_decode.txt", "w");
+	fpOut = fopen("output_encode.txt", "w");
 	if (fpOut != NULL)
 		fdOut = fileno(fpOut);
 	else {
-		fprintf(stderr, errmsg[1]);
+		fprintf(stderr, "Error: output_encode.txt.\n");
 		exit(1);
 	}
 
-	base64_decode(fdIn, fdOut);
+	base64_encode(fdIn, fdOut);
+
+	if (fpIn != NULL)
+		fclose(fpIn);
+
+	if (fpOut != NULL)
+		fclose(fpOut);
+
+	// fpIn = fopen("input_decode.txt", "r");
+	// if (fpIn != NULL)
+	// 	fdIn = fileno(fpIn);
+	// else {
+	// 	fprintf(stderr, "Error: input_decode.txt.\n");
+	// 	exit(1);
+	// }
+
+	// fpOut = fopen("output_decode.txt", "w");
+	// if (fpOut != NULL)
+	// 	fdOut = fileno(fpOut);
+	// else {
+	// 	fprintf(stderr, "Error: output_decode.txt.\n");
+	// 	exit(1);
+	// }
+
+	// base64_decode(fdIn, fdOut);
 
 	return 0;
 }
